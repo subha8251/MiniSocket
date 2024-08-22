5132dba8a1f900c191827c89feed6bb42f3cb0fc
subha8251 <116370185+subha8251@users.noreply.github.com>
2024-08-23 00:41:42 +0530

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,93 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+socket.emit('joinRoom', { username, room });
+
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+socket.on('message', (message) => {
+  console.log(message);
+  outputMessage(message,username);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let msg = e.target.elements.msg.value;
+
+  msg = msg.trim();
+
+  if (!msg) {
+    return false;
+  }
+
+  socket.emit('chatMessage', msg);
+
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+function outputMessage(message, currentUsername) {
+  const div = document.createElement('div');
+  
+  console.log(message.username);
+  console.log(currentUsername);
+
+  div.classList.add('message');
+
+  if (message.username === currentUsername) {
+    div.style.marginLeft = 'auto'; 
+    div.style.backgroundColor = '#ffffff';
+  } else {
+    div.style.marginRight = 'auto';
+    div.style.backgroundColor = '#dcf8c6' ;
+  }
+  
+
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>  ${message.time}</span>`;
+  div.appendChild(p);
+
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
